5519848f186eb6abd4a3b27e56d4af2f3f9f7076
Vincent REHM <vincentrehm@yahoo.fr>
2016-08-11 17:20:17 +0200

    mister cocktail mvp

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,40 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, :set_ingredients_collection, only: [:new, :create]
+  before_action :set_dose, only: [:destroy]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    # @cocktail.doses.build(dose_params)
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = @dose.cocktail
+    @dose.delete
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_ingredients_collection
+    @ingredients = Ingredient.all
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create] #do |variable|
-    #resources :reviews, only: [:new, :create]
-  #end
+  resources :cocktails, only: [:index, :show, :new, :create] do |variable|
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
